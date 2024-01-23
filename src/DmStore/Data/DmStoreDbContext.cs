d3617fc3f25620c4f4b1d0b1c3ac182dd21cb2b8
rhchaves <rodolfohenriquechaves@gmail.com>
2024-01-23 10:15:48 -0300

    Add project files.

---
+++ src/DmStore/Data/DmStoreDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace DmStore.Data
+{
+    public class DmStoreDbContext : IdentityDbContext
+    {
+        public DmStoreDbContext(DbContextOptions<DmStoreDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
